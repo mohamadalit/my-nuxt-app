e09a852de9c34fec67f58d080302b2f4eb9ddffd
mohamadalit <116593909+mohamadalit@users.noreply.github.com>
2026-04-27 17:57:04 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,19 @@
 import { i18nConfig } from './i18n/i18n.config'
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
 
   modules: [
-    '@nuxt/ui',
+    // '@nuxt/ui', // موقتاً غیرفعال چون با Nuxt 3 مشکل داشت
     '@nuxtjs/i18n',
     '@nuxtjs/supabase',
     '@hypernym/nuxt-anime',
     '@nuxt/eslint',
-    // 'nitro-cloudflare-dev',
     '@vueuse/nuxt',
-    // '@nuxthub/core',
   ],
 
   devtools: { enabled: false },
 
   css: ['~/assets/css/main.css'],
-
-  ui: {
-    theme: {
-      colors: [
-        'primary',
-        'secondary',
-        'brand',
-        'info',
-        'success',
-        'warning',
-        'error',
-      ],
-    },
-  },
 
   runtimeConfig: {
     public: {
@@ -48,10 +32,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-10-16',
 
   nitro: {
+    preset: 'node-server',
+
     prerender: {
-      routes: [
-        '/',
-      ],
+      routes: ['/'],
       ignore: [
         (path: string) => path.startsWith('/editor'),
         (path: string) => path.startsWith('/en/editor'),
@@ -59,6 +43,7 @@ export default defineNuxtConfig({
       ],
       crawlLinks: true,
     },
+
     storage: {
       r2: {
         driver: 's3',
@@ -67,40 +52,6 @@ export default defineNuxtConfig({
         endpoint: process.env.S3_API || '',
         bucket: 'public',
         region: 'auto',
-      },
-    },
-    preset: 'cloudflare-module',
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true,
-      wrangler: {
-        name: 'latelierdegz',
-        main: './.output/server/index.mjs',
-        // "compatibility_flags": ["nodejs_compat"],
-        compatibility_date: '2025-09-28',
-        assets: {
-          binding: 'ASSETS',
-          directory: './.output/public/',
-        },
-        r2_buckets: [{
-          binding: 'R2_BUCKET',
-          bucket_name: 'public',
-        }],
-        workers_dev: false, // set to false when using custom domain
-        observability: {
-          enabled: true,
-        },
-        vars: {
-          NUXT_PUBLIC_SITE_URL: process.env.NUXT_PUBLIC_SITE_URL,
-          NUXT_PUBLIC_API_URL: process.env.NUXT_PUBLIC_API_URL,
-          S3_API: process.env.S3_API,
-          SUPABASE_URL: process.env.SUPABASE_URL,
-          SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY,
-          SUPABASE_KEY: process.env.SUPABASE_KEY,
-          R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
-          R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
-          OPENAI_API_KEY: process.env.NUXT_OPENAI_API_KEY,
-        },
       },
     },
   },
